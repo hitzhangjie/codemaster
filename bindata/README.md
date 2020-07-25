432cc66b34f3385bdb87602a7fee5041cbdc3a81
hitzhangjie <hit.zhangjie@gmail.com>
2020-07-25 17:17:42 +0800

    bindata: 展示如何将文件或者目录转换成一个go文件来使用

--- bindata/README.md
+++ bindata/README.md
@@ -3,3 +3,18 @@ go似乎有计划要支持对静态资源文件的打包，但是目前最新go�
 如果想实现类似功能，就只能借助一些第三方的库，比如go-rice、go-bindata等等，这些三方库用起来也没那么简单方便。
 
 这里写了个简单的demo，后续有类似需求可以考虑这么实现，后面go官方支持了再考虑切换实现方式，没必要用那些过度设计的三方库了。
+
+**1. 演示下如何使用bindata来将文件或者整个目录转换成go文件存储**
+
+```go
+# 构建bindata工具
+go build -v
+
+# 将整个static目录压缩后存储到一个gobin/static.go文件
+./bindata -input static/ -output gobin/static.go
+```
+
+**2. 演示下如何在代码中使用上述生成的go文件来还原回以前的文件内容**
+
+- 代码中其他地方直接通过 `gobin.StaticGo` 来引用上述内容
+- 可以通过 `compress.UnTar(dst, ...)` 解压后当做本地文件使用
--- bindata/bindata.go
+++ bindata/bindata.go
@@ -1,1 +1,101 @@
-package bindata
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/hitzhangjie/codemaster/compress"
+	"github.com/iancoleman/strcase"
+)
+
+var (
+	input  = flag.String("input", "", "read data from input, which could be a regular file or directory")
+	output = flag.String("output", "", "write transformed data to named *.go, which could be linked with binary")
+	gopkg  = flag.String("gopkg", "gobin", "write transformed data to *.go, whose package is $package")
+)
+
+var tpl = `package {{.GoPackage}}
+var {{.Variable}} = []uint8{
+{{ range $idx, $val := .Data }}{{$val}},{{ end }}
+}`
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+
+	// 输入输出参数校验
+	if len(*input) == 0 || len(*gopkg) == 0 {
+		fmt.Println("invalid argument: invalid input")
+		os.Exit(1)
+	}
+
+	// 读取输入内容
+	buf, err := ReadFromInputSource(*input)
+	if err != nil {
+		fmt.Errorf("read data error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// 将内容转换成go文件写出
+	inputBaseName := filepath.Base(*input)
+	if len(*output) == 0 {
+		*output = fmt.Sprintf("%s_bindata.go", inputBaseName)
+	}
+
+	outputDir, outputBaseName := filepath.Split(*output)
+	tplInstance, err := template.New(outputBaseName).Parse(tpl)
+	if err != nil {
+		fmt.Printf("parse template error: %v\n", err)
+		os.Exit(1)
+	}
+	_ = os.MkdirAll(outputDir, 0777)
+
+	fout, err := os.OpenFile(*output, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Printf("open input error: %v", err)
+		os.Exit(1)
+	}
+
+	err = tplInstance.Execute(fout, &struct {
+		GoPackage string
+		Variable  string
+		Data      []uint8
+	}{
+		GoPackage: *gopkg,
+		Variable:  strcase.ToCamel(outputBaseName),
+		Data:      buf,
+	})
+	if err != nil {
+		panic(fmt.Errorf("template execute error: %v", err))
+	}
+
+	fmt.Printf("ok, filedata stored to %s\n", *output)
+}
+
+// ReadFromInputSource 从输入读取内容，可以是一个文件，也可以是一个目录（会先gzip压缩然后再返回内容）
+func ReadFromInputSource(inputSource string) (data []byte, err error) {
+
+	fin, err := os.Lstat(inputSource)
+	if err != nil {
+		return nil, err
+	}
+
+	if !fin.IsDir() {
+		return ioutil.ReadFile(*input)
+	}
+
+	buf := bytes.Buffer{}
+	err = compress.Tar(*input, &buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}

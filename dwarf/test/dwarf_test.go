45ad4e0865963052cb78a8df0250f07feb2fa3e7
hitzhangjie <hit.zhangjie@gmail.com>
2021-12-29 21:14:31 +0800

    testcase: read DWARF lineno table

--- dwarf/test/dwarf_test.go
+++ dwarf/test/dwarf_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/hitzhangjie/codemaster/dwarf/godwarf"
+	"github.com/hitzhangjie/codemaster/dwarf/line"
 	"github.com/hitzhangjie/codemaster/dwarf/reader"
 )
 
@@ -135,6 +136,7 @@ func Test_DWARFReadVariable(t *testing.T) {
 			continue
 		}
 		// 通过offset限制，只查看main.main中定义的变量名为s的变量
+		// 这里的0x432b9是结合`objdump --dwarf=info` 中的结果来硬编码的
 		if entry.Val(dwarf.AttrType).(dwarf.Offset) != dwarf.Offset(0x432b9) {
 			continue
 		}
@@ -144,12 +146,18 @@ func Test_DWARFReadVariable(t *testing.T) {
 		fmt.Println("DIE variable:", entry)
 
 		// 查看变量s对应的类型的DIE
-		ee, err := rd.SeekToType(entry, true, true)
+		variableTypeEntry, err := rd.SeekToType(entry, true, true)
 		assert.Nil(t, err)
-		fmt.Println("DIE type:", ee)
+		fmt.Println("DIE type:", variableTypeEntry)
 
 		// 查看变量s对应的地址 [lowpc, highpc, instruction]
 		fmt.Println("location:", entry.Val(dwarf.AttrLocation))
+
+		// 最后在手动校验下main.Student的类型与上面看到的变量的类型是否一致
+		// 应该满足：main.Student DIE的位置 == 变量的类型的位置偏移量
+		typeEntry, err := rd.SeekToTypeNamed("main.Student")
+		assert.Nil(t, err)
+		assert.Equal(t, typeEntry.Val(dwarf.AttrType), variableTypeEntry.Offset)
 		break
 	}
 }
@@ -177,5 +185,22 @@ func Test_DWARFReadFunc(t *testing.T) {
 }
 
 func Test_DWARFReadLineNoTable(t *testing.T) {
+	f, err := elf.Open("fixtures/elf_read_dwarf")
+	assert.Nil(t, err)
 
+	dat, err := godwarf.GetDebugSection(f, "line")
+	assert.Nil(t, err)
+
+	lineToPCs := map[int][]uint64{10: nil, 12: nil, 13: nil, 14: nil, 15: nil}
+
+	debuglines := line.ParseAll(dat, nil, nil, 0, true, 8)
+	fmt.Println(len(debuglines))
+	for _, line := range debuglines {
+		//fmt.Printf("idx-%d\tinst:%v\n", line.Instructions)
+		line.AllPCsForFileLines("/root/dwarftest/dwarf/test/fixtures/elf_read_dwarf.go", lineToPCs)
+	}
+
+	for line, pcs := range lineToPCs {
+		fmt.Printf("lineNo:[elf_read_dwarf.go:%d] -> PC:%#x\n", line, pcs)
+	}
 }

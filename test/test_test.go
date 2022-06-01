7e39a2002bcce0978caa8a03a2568bf1d9bbf69d
hitzhangjie <hit.zhangjie@gmail.com>
2022-06-01 20:31:41 +0800

    feat: compare strconv.Atoi with fmt.Sprintf

--- test/test_test.go
+++ test/test_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -14,4 +15,24 @@ func TestXXX(t *testing.T) {
 	// so following statements following t.Fatalf won't be executed
 	t.Fatalf("fatalf")
 	fmt.Println("helloworld2")
+}
+
+//BenchmarkSprintfInt2Str-8   	13052944	        83.63 ns/op
+func BenchmarkSprintfInt2Str(b *testing.B) {
+	var n int = 1024
+	var s string
+	for i := 1; i < b.N; i++ {
+		s = fmt.Sprintf("%d", n)
+		_ = s
+	}
+}
+
+//BenchmarkItoa2Str-8   	55205221	        19.01 ns/op
+func BenchmarkItoa2Str(b *testing.B) {
+	var n int = 1024
+	var s string
+	for i := 1; i < b.N; i++ {
+		s = strconv.Itoa(n)
+		_ = s
+	}
 }

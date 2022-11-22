3de3bfd8deea547e2d82c8e81f6176243438ec17
hitzhangjie <hit.zhangjie@gmail.com>
2022-11-23 02:14:18 +0800

    gc: ballast设置方案

---
+++ gc/ballast/ballast.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "time"
+)
+
+// 方法1
+//var ballast = make([]byte, 1<<30)
+
+// 方法2
+// var ballast []byte
+// func init() {
+//     ballast = make([]byte, 1<<30)
+// }
+
+func main() {
+    // 方法3
+    ballast := make([]byte, 1<<30)
+
+    // do something
+    // ...
+    time.Sleep(time.Minute)
+
+    runtime.KeepAlive(&ballast)
+}

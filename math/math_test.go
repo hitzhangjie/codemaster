b97d26b9acafb1d21c338d513ac62c57642d1062
hitzhangjie <hit.zhangjie@gmail.com>
2023-03-03 02:01:27 +0800

    docs/math: 整理了数值计算溢出的检测\解决办法,并介绍了如何通过fuzztest来发现问题

--- math/math_test.go
+++ math/math_test.go
@@ -2,6 +2,9 @@ package math_test
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"sync/atomic"
 	"testing"
 )
 
@@ -16,10 +19,18 @@ func Fuzz_overflow_int32(f *testing.F) {
 }
 
 func Fuzz_overflow_uint32(f *testing.F) {
+	// 此输入下，mutator无能为力，执行了几分钟也发现不了问题，如果知道mutator原理的就很容易明白为什么。
+	// 然后，进一步可以纠正下错误的测试思想，模糊测试!=漫无目的的测试，seed scorpus matters!
+	//
+	//f.Add(uint32(0xffffffff-1000), uint32(0))
 	f.Add(uint32(0xffffffff), uint32(0))
+	var count atomic.Int32
 	f.Fuzz(func(t *testing.T, a, b uint32) {
+		iter := count.Add(1)
 		if c, err := safeUnsignedAdd(a, b); err != nil {
-			t.Errorf("%v + %v = %v, err: %v", a, b, c, err)
+			t.Errorf("iter-%d %v + %v = %v, err: %v", iter, a, b, c, err)
+		} else {
+			t.Logf("iter-%d %v + %v = %v", iter, a, b, c)
 		}
 	})
 }
@@ -28,6 +39,7 @@ var errOverflow = errors.New("overflow")
 
 func safeSignedAdd[T ~int | ~int32 | ~int64](a, b T) (T, error) {
 	c := a + b
+	fmt.Fprintf(os.Stderr, "%v + %v = %v\n", a, b, c)
 	if a > 0 && b > 0 && c <= 0 ||
 		a < 0 && b < 0 && c >= 0 {
 		return c, errOverflow
@@ -37,6 +49,7 @@ func safeSignedAdd[T ~int | ~int32 | ~int64](a, b T) (T, error) {
 
 func safeUnsignedAdd[T ~uint | ~uint32 | ~uint64](a, b T) (T, error) {
 	c := a + b
+	fmt.Fprintf(os.Stderr, "%v + %v = %v\n", a, b, c)
 	if c < a || c < b {
 		return c, errOverflow
 	}

6dc38d3f40fbcf5936fd736d5f750e672be4bcc5
hitzhangjie <hit.zhangjie@gmail.com>
2023-08-16 10:16:16 +0800

    test/serialization: 完善一下测试用例,根据自定义struct tag生成大json

--- serialization/benchmark/fuzz_test.go
+++ serialization/benchmark/fuzz_test.go
@@ -3,6 +3,8 @@ package benchmark
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -31,8 +33,6 @@ type ContactInfo struct {
 }
 
 func Test_Generate(t *testing.T) {
-	slice := reflect.MakeSlice(reflect.TypeOf([]Person{}), 0, 3)
-	rt := reflect.TypeOf(Person{})
 	// repeat: 10, 4k
 	// repeat: 20, 10k
 	// repeat: 50, 24k
@@ -40,11 +40,27 @@ func Test_Generate(t *testing.T) {
 	// repeat: 200, 98k
 	// repeat: 500, 245k
 	// repeat: 1000, 491k
-	for repeat := 0; repeat < 1000; repeat++ {
-		//slice = reflect.Append(slice, newInstance(rt))
-		slice = reflect.Append(slice, newInstance(rt))
+	repeatTimes := []int{10, 20, 50, 100, 200, 500, 1000}
+	for _, repeatTime := range repeatTimes {
+		slice := reflect.MakeSlice(reflect.TypeOf([]Person{}), 0, 3)
+		rt := reflect.TypeOf(Person{})
+
+		for i := 0; i < repeatTime; i++ {
+			//slice = reflect.Append(slice, newInstance(rt))
+			slice = reflect.Append(slice, newInstance(rt))
+		}
+		//spew.Dump(slice.Interface())
+		buf, err := json.Marshal(slice.Interface())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("repeatTimes: %d size: %d-KB\n", repeatTime, len(buf)/1024)
+		fp, err := filepath.Abs(fmt.Sprintf("testdata/%d-KB.gen.json", len(buf)/1024))
+		if err != nil {
+			panic(err)
+		}
+		if err := os.WriteFile(fp, buf, os.ModePerm); err != nil {
+			panic(err)
+		}
 	}
-	//spew.Dump(slice.Interface())
-	buf, _ := json.Marshal(slice.Interface())
-	fmt.Println("size:", len(buf))
 }

bc6d703bff3220239dad2937136e0875cc3dadaa
hitzhangjie <hit.zhangjie@gmail.com>
2024-09-09 00:02:37 +0800

    registry/etcd: use etcd as service registry
    this CL adds a way to get hugerange keys by pagination, like:
    
    Get(ctx, key, opts...)
      - option WithRev, specify the same revision as the one first Get(...) returns
      - option WithLimit, specify the number of keys returned one time
      - option WithRange, specify the end key if you could
    
    From etcd issue list, we know: the hugerange requests may prevent the
    write requests in earlier etcdv3 versions, it may lead to a problem,
    that is the `applied index - commit index > 5000`, while 5000 is a
    upperlimit, if this limit is hit, it means the etcd cannot quickly
    finish the RAFT algorithm to keep the follower quickly follow up the
    master. The etcd cluster is in a bad state. So etcd may drop some
    requests to recover the state... Then, the client may encounter
    keepalive error, and have to re-initialize the etcd client later.

--- go.mod
+++ go.mod
@@ -1,20 +1,20 @@
 module github.com/hitzhangjie/codemaster
 
-go 1.18
+go 1.22.4
 
 require (
 	github.com/RoaringBitmap/roaring v0.9.4
 	github.com/bruceshao/lockfree v1.0.3
 	github.com/bytedance/sonic v1.10.0
-	github.com/cespare/xxhash/v2 v2.1.2
+	github.com/cespare/xxhash/v2 v2.2.0
 	github.com/davecgh/go-spew v1.1.1
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f
 	github.com/ftrvxmtrx/fd v0.0.0-20150925145434-c6d800382fff
 	github.com/go-mysql-org/go-mysql v1.6.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/goccy/go-json v0.10.2
-	github.com/golang/protobuf v1.5.2
-	github.com/google/uuid v1.3.0
+	github.com/golang/protobuf v1.5.4
+	github.com/google/uuid v1.3.1
 	github.com/iancoleman/strcase v0.2.0
 	github.com/influxdata/tdigest v0.0.1
 	github.com/json-iterator/go v1.1.12
@@ -28,11 +28,12 @@ require (
 	github.com/segmentio/encoding v0.3.6
 	github.com/smartystreets-prototypes/go-disruptor v0.0.0-20200316140655-c96477fd7a6a
 	github.com/spaolacci/murmur3 v1.1.0
-	github.com/stretchr/testify v1.8.1
+	github.com/stretchr/testify v1.9.0
 	github.com/v2pro/plz v0.0.0-20200805122259-422184e41b6e
 	github.com/zeebo/xxh3 v1.0.2
 	github.com/zeromicro/go-zero v1.3.5
 	github.com/zhenjl/cityhash v0.0.0-20131128155616-cdd6a94144ab
+	go.etcd.io/etcd/client/v3 v3.5.15
 	go.opentelemetry.io/otel v1.8.0
 	go.opentelemetry.io/otel/metric v0.30.0
 	go.opentelemetry.io/otel/sdk v1.8.0
@@ -40,18 +41,21 @@ require (
 	go.uber.org/atomic v1.9.0
 	go.uber.org/zap v1.24.0
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670
-	golang.org/x/net v0.0.0-20220531201128-c960675eff93
-	google.golang.org/grpc v1.47.0
+	golang.org/x/net v0.23.0
+	google.golang.org/grpc v1.59.0
 	google.golang.org/grpc/examples v0.0.0-20211119181224-d542bfcee46d
-	google.golang.org/protobuf v1.28.0
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
 	github.com/bits-and-blooms/bitset v1.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -64,11 +68,15 @@ require (
 	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
 	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.15 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.15 // indirect
 	go.opentelemetry.io/otel/trace v1.8.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220602131408-e326c6e8e9c8 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ registry/etcd_test.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+func Test_etcd_register(t *testing.T) {
+	// init etcdclient
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:         []string{"http://localhost:2379"},
+		DialTimeout:       time.Second,
+		DialKeepAliveTime: time.Second,
+	})
+	require.Nil(t, err)
+
+	// create a lease
+	lease, err := client.Grant(context.Background(), 10)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("lease created: lease=%v, ttl=%d, revision=%d", lease.ID, lease.TTL, lease.Revision)
+
+	// start keepalive
+	go keepalive(t, client, lease.ID)
+
+	// provided some service instances started and register into etcd registry
+	numOfInstances := 10000
+	prepareServiceInstances(t, client, lease.ID, numOfInstances)
+
+	// provided every service to fetch the instances list from registry
+	startServiceInstances(t, client, lease.ID)
+
+	time.Sleep(time.Second * 20)
+}
+
+func keepalive(t *testing.T, client *clientv3.Client, lease clientv3.LeaseID) {
+	ch, err := client.KeepAlive(context.Background(), lease)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		_, sentBeforeClosed := <-ch
+		if !sentBeforeClosed {
+			break
+		}
+	}
+
+	t.Logf("etcd keep alive failed, lease:%v", lease)
+}
+
+func prepareServiceInstances(t *testing.T, client *clientv3.Client, lease clientv3.LeaseID, total int) {
+	opts := []clientv3.OpOption{
+		clientv3.WithLease(lease),
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(total)
+	begin := time.Now()
+	for i := 0; i < total; i++ {
+		go func() {
+			defer wg.Done()
+			key := fmt.Sprintf("/myname/%d", i)
+			val := fmt.Sprintf("/myvalue/%d", i)
+			rsp, err := client.Put(context.Background(), key, val, opts...)
+			if err != nil {
+				t.Errorf("put kvpair failed: err=%v", err)
+			}
+			t.Logf("put kvpair ok: index=%d, key=%s, revision=%+v", i, key, rsp.Header.Revision)
+		}()
+	}
+	wg.Wait()
+	t.Logf("put %d kvpairs ... %v", total, time.Since(begin))
+}
+
+func startServiceInstances(t *testing.T, client *clientv3.Client, lease clientv3.LeaseID) {
+	for dsaInstanceIdx := 1; dsaInstanceIdx <= 10000; dsaInstanceIdx++ {
+		go func() {
+			// get others
+			revision := getbyprefix(t, client)
+
+			// watch others since revision
+			go func() {
+				ch := client.Watch(context.TODO(), "/myname",
+					clientv3.WithPrefix(),
+					clientv3.WithRev(revision))
+				for msg := range ch {
+					for _, evt := range msg.Events {
+						t.Logf("watchevent: type=%v key=%s val=%s", evt.Type, evt.Kv.Key, evt.Kv.Value)
+					}
+				}
+			}()
+
+			// register itself
+			myself := fmt.Sprintf("/myself-%d", dsaInstanceIdx)
+			_, err := client.Put(context.Background(), myself, "value", clientv3.WithLease(lease))
+			if err != nil {
+				t.Logf("register myself fail: key=%s, err=%v", myself, err)
+				time.Sleep(time.Second / 10000)
+			}
+		}()
+	}
+}
+
+func getbyprefix(t *testing.T, client *clientv3.Client) int64 {
+	var revision int64 = 0
+	var page = 1
+	var key = "/"
+	var end = "/zzzzzz"
+
+	opts := []clientv3.OpOption{
+		clientv3.WithPrefix(),
+		clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend),
+		clientv3.WithRange(end),
+		clientv3.WithLimit(100),
+	}
+
+	for {
+		rsp, err := client.Get(context.Background(), key, opts...)
+		if err != nil {
+			t.Logf("Failed to get key-value pair in etcd, error: %v", err)
+			continue
+		}
+		if revision == 0 {
+			opts = append(opts, clientv3.WithRev(rsp.Header.Revision))
+		}
+
+		for _, kv := range rsp.Kvs {
+			t.Logf("getbyprefix: page=%d key=%s", page, kv.Key)
+			key = string(kv.Key)
+		}
+		if !rsp.More {
+			break
+		}
+		page++
+		time.Sleep(time.Millisecond * 10)
+	}
+	return revision
+}

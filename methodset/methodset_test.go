8a3bf11e507d197374275f5391ec11d31026c945
hitzhangjie <hit.zhangjie@gmail.com>
2025-03-11 01:54:01 +0800

    pk: 对比go、rust处理优劣

--- methodset/methodset_test.go
+++ methodset/methodset_test.go
@@ -7,8 +7,15 @@ type student struct {
 	name string
 }
 
+// 我们写这个函数，实际上编译器干了什么呢？
 func (s student) SetAge(v int) {
+	// 检测到ineffective assign，有助于发现这里的赋值可能存在问题，但是并不总是奏效，比如我加一个 _ = s.age后，就不会再报错。
+	// 所以 …… 这里的写法 s.age = v 并没有靠谱的办法被检查出来，这里修改的s的拷贝的字段，而非s的字段
+	//
+	// 对于go语言设计者来说，一个小struct的拷贝比一个指针容易减轻GC开销，如果struct比较小，且没有对其进行修改的诉求，建议用value接收器类型，
+	// 反之用pointer接收器类型。
 	s.age = v
+	// _ = s.age
 }
 
 func TestHelloWorld(t *testing.T) {
